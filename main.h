27ddb4cc2bedd54f68619beda7fbf2c1d3cc0767
Moses <mosemuthoni57@gmail.com>
2023-05-18 21:10:58 +0300

    Creating Header File

--- main.h
+++ main.h
@@ -2,8 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 char *get_line();
 int _strcmp(char *s1, char *s2);
 unsigned int _strspn(char *s, char *accept);
