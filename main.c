4a04019e82d6f26b274bc4dd05dc045520ce7e66
Moses <mosemuthoni57@gmail.com>
2023-05-18 11:07:30 -0700

    Simple shell

--- main.c
+++ main.c
@@ -3,18 +3,19 @@
  * main - the main function
  * Return: 0 on success
  */
-int main()
+int main(void)
 {
 	int fk, i;
 	char *input, *arg[3];
 	int result;
+
 	while (1)
 	{
 		printf("($) ");
 		input = get_line();
 		arg[0] = input;
 		arg[1] = NULL;
-		if (_strcmp(input, "exit") == 0) 
+		if (_strcmp(input, "exit") == 0)
 		{
 			free(input);
 			exit(0);
@@ -23,12 +24,12 @@ int main()
 		if (fk == 0)
 		{
 			result = execve(arg[0], arg, NULL);
-			if (result == -1) 
+			if (result == -1)
 				printf("Error executing command.\n");
 			free(input);
 		}
 		else if (fk > 0)
 			wait(NULL);
 	}
-	return 0;
+	return (0);
 }
